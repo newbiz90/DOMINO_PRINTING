7e1594ecbed6e03258826c2c2d35e204111dd751
Matthew Oh <newbiz90@hotmail.com>
2024-03-21 16:19:13 +0800

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+# Create an admin user
+admin_user = User.create!(
+  email: 'admin@example.com',
+  role: 1,  # Assign the role as an integer
+  password: 'password',
+  password_confirmation: 'password',
+  created_at: Time.zone.now,
+  updated_at: Time.zone.now
+)
+
+# Create a regular user
+regular_user = User.create!(
+  email: 'user@example.com',
+  role: 0,  # Assign the role as an integer
+  password: 'password',
+  password_confirmation: 'password',
+  created_at: Time.zone.now,
+  updated_at: Time.zone.now
+)
+
+puts "Seed data created successfully!"
