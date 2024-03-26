b925d1016fd162e8b33091e7691a8dc9c3517460
Matthew Oh <newbiz90@hotmail.com>
2024-03-26 14:48:05 +0800

    project tracking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    # You can render your index.html.erb here
+    # Example: render 'index'
   end
 end
